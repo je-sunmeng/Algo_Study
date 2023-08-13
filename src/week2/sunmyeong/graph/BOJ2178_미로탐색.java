176d39957415bcee14f58a360a0a071cd7b2c869
je-sunmeng <jesunmyeong@gmail.com>
2023-08-13 15:15:12 +0900

    FEAT BOJ2178미로탐색 fin

--- src/week2/sunmyeong/graph/BOJ2178_미로탐색.java
+++ src/week2/sunmyeong/graph/BOJ2178_미로탐색.java
@@ -1,5 +1,6 @@
 /*
- * 시간초과
+ * 메모리 	12204KB
+ * 실행시간 84ms
  */
 
 package week2.sunmyeong.graph;
@@ -8,18 +9,31 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ2178_미로탐색 {
-	static int N, M, min = Integer.MAX_VALUE;
-	static boolean map[][];
+	//상 하 좌 우
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	static class Pos{
+		int row;
+		int col;
+		int rank;
+		public Pos(int row, int col, int rank) {
+			this.row = row;
+			this.col = col;
+			this.rank = rank;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		StringTokenizer stk = new StringTokenizer(str);
-		N = Integer.parseInt(stk.nextToken());
-		M = Integer.parseInt(stk.nextToken());
-		int row = 0, col = 0, rank = 1;
+		int N = Integer.parseInt(stk.nextToken());
+		int M = Integer.parseInt(stk.nextToken());
+		boolean[][] map = new boolean[N][M];	//true:길, false:벽
+		int row, col, nr, nc;
 		
-		map = new boolean[N][M];	//true:길, false:벽
-		ArrayDeque<int[]> BFS = new ArrayDeque<>();
+		Queue<Pos> BFS = new LinkedList<>();	//BFS를 위한 Queue
 		
 		for(int i = 0; i < N; i++) {
 			str = br.readLine();
@@ -30,29 +44,26 @@ public class BOJ2178_미로탐색 {
 			}
 		}
 		
-		BFS.add(new int[] {row,col,rank});
-		
+		Pos now = null;
+		BFS.add(new Pos(0, 0, 1));
+				
 		while(true) {
-			int[] now = BFS.poll();
-			row = now[0];
-			col = now[1];
-			rank = now[2];
-
+			now = BFS.poll();
+			row = now.row;
+			col = now.col;
 			if(row == N-1 && col == M-1) {
-				min = rank;
 				break;
 			}
-
-			map[row][col] = false;
-
-			if(row < N-1 && map[row+1][col]) BFS.add(new int[] {row+1, col, rank+1});	//아래
-			if(col < M-1 && map[row][col+1]) BFS.add(new int[] {row, col+1, rank+1});	//오른쪽
-			if(row > 0 && map[row-1][col]) BFS.add(new int[] {row-1, col, rank+1});	//위
-			if(col > 0 && map[row][col-1]) BFS.add(new int[] {row, col-1, rank+1});	//왼쪽
-			
+//			map[row][col] = false; // 이곳에 넣으면 메모리 초과
+			for(int dir = 0; dir < 4; dir++) {
+				nr = row+dr[dir];
+				nc = col+dc[dir];
+				if(nr<0||nr>N-1||nc<0||nc>M-1||!map[nr][nc]) continue;
+				map[nr][nc] = false;	//이곳에 넣어야 메모리 초과가 나지 않음
+				BFS.add(new Pos(nr, nc, now.rank+1));
+			}
 		}
 		
-		System.out.println(min);		
+		System.out.println(now.rank);		
 	}
-
 }
