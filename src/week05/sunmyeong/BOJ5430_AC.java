b3075ae83b21334ddfc42a21d654b7c2b907e102
sunmeng <jesunmyeong@gmail.com>
2023-08-31 22:58:27 +0900

    FEAT BOJ5430_AC fin

--- src/week05/sunmyeong/BOJ5430_AC.java
+++ src/week05/sunmyeong/BOJ5430_AC.java
@@ -1,3 +1,8 @@
+/*
+ * 메모리	123496KB
+ * 실행시간 928ms
+ */
+
 package week05.sunmyeong;
 
 import java.io.*;
@@ -5,13 +10,17 @@ import java.util.*;
 
 public class BOJ5430_AC {
 	
+	public static int countChar(String str, char ch) {
+		return str.length() - str.replace(String.valueOf(ch), "").length();
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine()); // 테케수 입력
-		Deque<Integer> nums = new ArrayDeque<>();
+		List<Integer> nums = new ArrayList<>();
 		
-		go:for(int test = 1; test <= T; test++) {
+		for(int test = 1; test <= T; test++) {
 			String com = br.readLine();	// RD명령어 입력
 			int len = Integer.parseInt(br.readLine());	// 숫자 개수 입력
 			String str = br.readLine();	// 숫자 입력
@@ -23,6 +32,21 @@ public class BOJ5430_AC {
 			for(int i = 0; i < len; i++) {
 				nums.add(Integer.parseInt(stk.nextToken()));
 			}
+			int[] index = {0, nums.size()-1};
+			
+			int D = countChar(com, 'D');
+			
+			if(D > nums.size()) {
+				sb.append("error").append("\n");
+				nums.clear();
+				continue;
+			}
+			
+			if(D == len || len == 0) {
+				sb.append("[").append("]").append("\n");
+				nums.clear();
+				continue;
+			}
 			
 			for(int c = 0; c < com.length(); c++) {
 				char command = com.charAt(c);
@@ -30,27 +54,25 @@ public class BOJ5430_AC {
 					start ^= 1;
 				}
 				else {
-					if(nums.size() == 0) {
-						sb.append("error").append("\n");
-						continue go;
-					}
-					if(start == 0) nums.pollFirst();
-					else nums.pollLast();
+					if(start == 0) index[0]++;
+					else index[1]--;
 				}
 			}
 			
 			sb.append("[");
 			if(start == 0) {
-				while(nums.size() != 1) {
-					sb.append(nums.pollFirst()).append(",");
+				for(int i = index[0]; i < index[1]; i++) {
+					sb.append(nums.get(i)).append(",");
 				}
+				sb.append(nums.get(index[1]));
 			}
 			else {
-				while(nums.size() != 1) {
-					sb.append(nums.pollLast()).append(",");
+				for(int i = index[1]; i > index[0]; i--) {
+					sb.append(nums.get(i)).append(",");
 				}
+				sb.append(nums.get(index[0]));
 			}
-			sb.append(nums.poll()).append("]").append("\n");
+			sb.append("]").append("\n");
 			nums.clear();
 		}
 		System.out.println(sb);
