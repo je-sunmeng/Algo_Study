0d85b7f349d6aab3f53675035f503ba53a9a9bc1
je-sunmeng <jesunmyeong@gmail.com>
2023-09-19 17:47:11 +0900

    .

--- src/week08/sunmyeong/BOJ1325_효율적인해킹.java
+++ src/week08/sunmyeong/BOJ1325_효율적인해킹.java
@@ -4,49 +4,53 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ1325_효율적인해킹 {
-	
-	static int parents[], N;
-	
-	public static void make() {
-		parents = new int[N];
-		for(int i = 0; i < N; i++) {
-			parents[i] = i;
-		}
-	}
-	
-	public static int find(int a) {
-		if(a == parents[a]) return a;
-		return parents[a] = find(parents[a]);
-	}
-	
-	public static boolean union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
-		if(rootA == rootB) return false;
-		parents[rootB] = rootA;
-		return true;
-	}
-	
+
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		StringTokenizer stk = new StringTokenizer(str);
 		
-		N = Integer.parseInt(stk.nextToken());
+		int N = Integer.parseInt(stk.nextToken());
 		int M = Integer.parseInt(stk.nextToken());
-		make();
-		int[] cnt = new int[N];
+
+		List<Integer> adjList[] = new ArrayList[N];
+		for(int i = 0; i < N; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
 		for(int i = 0; i < M; i++) {
 			str = br.readLine();
 			stk = new StringTokenizer(str);
 			int A = Integer.parseInt(stk.nextToken())-1;
 			int B = Integer.parseInt(stk.nextToken())-1;
-			union(A, B);
+			adjList[B].add(A);
 		}
+		
+		int[] cnts = new int[N];
+		int max = 0;
 		for(int i = 0; i < N; i++) {
-			System.out.print(parents[i] + " ");
+			boolean[] visited = new boolean[N];
+			int cnt = 0;
+			Queue<Integer> que = new ArrayDeque<>();
+			que.offer(i);
+			visited[i] = true;
+			while(!que.isEmpty()) {
+				int nxt = que.poll();
+				cnt++;
+				for(int j = 0; j < adjList[nxt].size(); j++) {
+					if(visited[adjList[nxt].get(j)]) continue;
+					que.offer(adjList[nxt].get(j));
+				}
+			}
+			cnts[i] = cnt;
+			if(max < cnt) max = cnt;
 		}
+		
+		for(int i = 0; i < N; i++) {
+			if(cnts[i]==max) System.out.print((i+1) + " ");
+		}
+
 
 	}
 
